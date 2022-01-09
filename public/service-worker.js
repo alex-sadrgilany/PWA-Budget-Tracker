6c1e593cdaa13522e2681e04c7fc0f94051557ee
AS <asadrgilany7@gmail.com>
2022-01-08 21:54:36 -0500

    rewrote some of the service worker and added idb.js to the html file

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,5 @@
 const FILES_TO_CACHE = [
+    "/",
     "./index.html",
     "./manifest.json",
     "./css/styles.css",
@@ -26,6 +27,8 @@ self.addEventListener("install", function(event) {
                 return cache.addAll(FILES_TO_CACHE);
             })
     );
+
+    self.skipWaiting();
 });
 
 self.addEventListener("activate", function(event) {
@@ -47,21 +50,45 @@ self.addEventListener("activate", function(event) {
                 );
             })
     );
+
+    self.clients.claim();
 });
 
 self.addEventListener("fetch", function(event) {
-    console.log("fetch request: " + event.request.url);
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(CACHE_NAME)
+                .then(cache => {
+                    return fetch(event.request)
+                        .then(response => {
+                            if (response.status === 200) {
+                                cache.put(event.request.url, response.clone());
+                            }
+
+                            return response;
+                        })
+                        .catch(err => {
+                            return cache.match(event.request);
+                        });
+                })
+                .catch(err => console.log(err))
+        );
+
+        return;
+    };
+
     event.respondWith(
-        caches.match(event.request)
-            .then(function(request) {
-                if (request) {
-                    console.log("responding with cache: " + event.request.url);
-                    return request;
-                }
-                else {
-                    console.log("file is not cached, fetching: " + event.request.url);
-                    return fetch(event.request);
-                };
+        fetch(event.request)
+            .catch(() => {
+                return caches.match(event.request)
+                    .then(response => {
+                        if (response) {
+                            return response;
+                        }
+                        else if (event.request.headers.get("accept").includes("text/html")) {
+                            return caches.match("/");
+                        }
+                    });
             })
     );
 });
